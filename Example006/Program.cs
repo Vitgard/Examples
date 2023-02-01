b9f913063d262f27aa9e68935a3938d12b065ff1
Krasnov Vitaliy <vitgard82@mail.ru>
2023-02-01 16:46:44 +0600

    Решение задачи 6

---
+++ Example006/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдает, является ли число четным
+Console.Clear();
+
+Console.Write("Введите число: ");
+int even = int.Parse(Console.ReadLine()!);
+if (even % 2 != 0)
+{
+    Console.Write("Данное число нечетное");
+}
+else
+{
+    Console.Write("Данное число четное");
+}

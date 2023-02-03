0bfe9ffc3306ed8941c056db795e5e6cc3a18807
Krasnov Vitaliy <vitgard82@mail.ru>
2023-02-03 15:37:58 +0600

    Решение задачи 2 семинара 2

---
+++ Example009_2sem/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Clear();
+
+int num = new Random().Next(100, 1000);
+
+Console.WriteLine($"Рандомное число {num}");
+Console.WriteLine($"Получилось число {num / 100 * 10 + num % 10}");
+
+
+

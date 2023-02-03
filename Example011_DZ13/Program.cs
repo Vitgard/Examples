fcc0d79e0aa88abdea14123e7d87680f710748cd
Krasnov Vitaliy <vitgard82@mail.ru>
2023-02-03 16:46:16 +0600

    Решение задачи 13 семинара 2

---
+++ Example011_DZ13/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int number1 = int.Parse(Console.ReadLine()!);
+int number2 = number1 % 10;
+if (number1 >= 100 && number1 < 1000)
+    Console.WriteLine($"Третья цифра введенного числа {number2}");
+else
+    Console.WriteLine("Третьей цифры нет");
+
+
+
+

25f35bafcc5ef5b953b419294b7a5af398ae3223
Krasnov Vitaliy <vitgard82@mail.ru>
2023-02-04 11:11:43 +0600

    Нашел максимум из девяти цифр массива

---
+++ Example014_lec2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Находим максимум из девяти цифр массива
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+int[] array = { 11, 5, 65, 34, 5556, 90, 3, 52, 943 };
+int result = Max(
+    Max(array[0], array[1], array[2]),
+    Max(array[3], array[4], array[5]),
+    Max(array[6], array[7], array[8])
+);
+Console.WriteLine(result);
+
+
+
+
+
+
+
+

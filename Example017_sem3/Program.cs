0019c97523751b6762b640e1d0ec87733d817bd1
Krasnov Vitaliy <vitgard82@mail.ru>
2023-02-04 14:01:30 +0600

    Определение координат по четверти

---
+++ Example017_sem3/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+Console.Clear();
+
+Console.Write("Введите четверть: ");
+int a = int.Parse(Console.ReadLine());
+
+if (a == 1)
+{
+    Console.WriteLine("x > 0, y > 0");
+}
+else if (a == 2)
+{
+    Console.WriteLine("x < 0, y > 0");
+}
+else if (a == 3)
+{
+    Console.WriteLine("x < 0, y < 0");
+}
+else if (a == 4)
+{
+    Console.WriteLine("x > 0, y < 0");
+}
+else
+{
+    Console.WriteLine("Введите корректное число четверти");
+}

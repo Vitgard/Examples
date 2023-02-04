85ba8b0c44c08b408731c6fd45665c78bebe9449
Krasnov Vitaliy <vitgard82@mail.ru>
2023-02-04 14:34:19 +0600

    Таблица квадратов чисел

---
+++ Example018_sem3/Program.cs
@@ -0,0 +1,19 @@
+﻿/// Напишите программу, которая принимает на вход
+//число (N) и выдаёт таблицу квадратов чисел
+//от 1 до N.
+
+Console.Clear();
+
+int count = 1;
+Console.Write("введте число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+while (count <= a)
+{
+    Console.Write(count * count);
+    if (count != a)
+    {
+        Console.Write(", ");
+    }
+    count++;
+}

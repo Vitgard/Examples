17f78178b87d61e88f0717a53ec02162ba1c0861
Krasnov Vitaliy <vitgard82@mail.ru>
2023-02-05 19:10:01 +0600

    Сумма чисел от 1 до N

---
+++ Example019_sem4/Program.cs
@@ -0,0 +1,24 @@
+﻿// Найдите сумму числе от 1 до N
+Console.Clear();
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine()!);
+int sum = 0;
+if (num > 0)
+{
+    for (int i = 0; i <= num; i++) 
+    {
+        sum = sum + i;
+    }
+}
+else
+{
+    for (int i = 0; i >= num; i--) 
+    {
+        sum = sum + i;
+    }
+}
+Console.WriteLine(sum);
+
+
+

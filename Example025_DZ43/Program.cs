37727fc0047e478a397a3e5e7745a7f5bd426748
Krasnov Vitaliy <vitgard82@mail.ru>
2023-02-11 10:26:15 +0600

    Подсчитал произведение чисел от 1 до N

---
+++ Example025_DZ43/Program.cs
@@ -0,0 +1,18 @@
+﻿//Написать программу вычисления произведения чисел от 1 до N
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine()!);
+int sum = 1;
+
+    for (int i = 1; i <= N; i++) 
+    {
+        sum = sum * i;
+    }
+
+Console.WriteLine(sum);
+
+
+
+

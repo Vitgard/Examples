62b040df908fb3029e42ab5836c79d64c3c92100
Krasnov Vitaliy <vitgard82@mail.ru>
2023-02-12 10:33:52 +0600

    Определил есть ли заданное число в массиве

---
+++ Example028_sem5/Program.cs
@@ -0,0 +1,31 @@
+﻿//Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве
+Console.Clear();
+int[] GetArray(int size, int minValue, int maxValue)
+{
+int[] result = new int[size];
+
+for (int i = 0; i < size; i++)
+{
+result[i] = new Random().Next(minValue, maxValue + 1);
+}
+return result;
+}
+
+bool FindNumber(int[] inArray, int number)
+{
+foreach (int element in inArray)
+{
+if (element == number)
+return true;
+}
+return false;
+}
+
+int[] arr = GetArray(5, -10, 10);
+Console.WriteLine(String.Join(", ", arr));
+int chislo = int.Parse(Console.ReadLine());
+if (FindNumber(arr, chislo))
+{
+Console.WriteLine("Да");
+}
+else Console.WriteLine("Нет");

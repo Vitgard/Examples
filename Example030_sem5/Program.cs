4be88324a68a28365fa42e5cfb4bda81f2f766e6
Krasnov Vitaliy <vitgard82@mail.ru>
2023-02-12 11:04:26 +0600

    Произведения пар в массиве

---
+++ Example030_sem5/Program.cs
@@ -0,0 +1,40 @@
+﻿// Парой считаем первый и последний элемент, второй и предпоследний
+// и т.д. Результат запишите в новом массиве.
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+int[] result = new int[size];
+
+for (int i = 0; i < size; i++)
+{
+result[i] = new Random().Next(minValue, maxValue + 1);
+}
+return result;
+}
+
+int[] MultiArray(int[] array)
+{
+int size = array.Length / 2;
+if (array.Length % 2 == 1)
+{
+size++;
+}
+int[] result = new int[size];
+for (int i = 0; i < array.Length / 2; i++)
+{
+result[i] = array[i] * array[array.Length - 1 - i];
+}
+if (array.Length % 2 == 1)
+{
+result[size - 1] = array[array.Length / 2];
+}
+return result;
+}
+
+Console.Clear();
+
+int[] array = GetArray(4, 0, 10);
+Console.WriteLine(String.Join(", ", array));
+
+int[] multiArray = MultiArray(array);
+Console.WriteLine(String.Join(", ", multiArray));

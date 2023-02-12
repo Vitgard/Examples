822dc06170fa54144b7e2efd210f6d7ce52214ca
Krasnov Vitaliy <vitgard82@mail.ru>
2023-02-12 10:49:12 +0600

    Нашел количество элементов массива

---
+++ Example029_sem5/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задайте одномерный массив из 123 случайных чисел.
+// Найдите количество элементов массива, значения которых лежат в
+// отрезке [10,99].
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+int[] result = new int[size];
+
+for (int i = 0; i < size; i++)
+{
+result[i] = new Random().Next(minValue, maxValue + 1);
+}
+return result;
+}
+
+int CountNumbers(int[] array)
+{
+int count = 0;
+foreach (int elent in array)
+{
+if (elent >= 10 && elent <= 99 )
+{
+count++;
+}
+}
+return count;
+}
+
+int[] array = GetArray(123, 0, 150);
+Console.WriteLine(String.Join(", ", array));
+int count = CountNumbers(array);
+Console.WriteLine(count);

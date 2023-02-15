b64a76e61d6715e5c1442e3def3ef4de2641e8c3
Krasnov Vitaliy <vitgard82@mail.ru>
2023-02-15 11:25:56 +0600

    исправил ошибку

---
+++ Example032_DZ37/Program.cs
@@ -0,0 +1,29 @@
+﻿//  Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+Console.Clear();
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return result;
+}
+int Result(int[] inArray)
+{
+    int count = 0;
+    for (int i = 1; i < inArray.Length; i++)
+    {
+        if (inArray[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+int[] array = GetArray(10, 100, 999);
+Console.WriteLine(String.Join(", ", array));
+int count = Result(array);
+Console.WriteLine(count);
+
+

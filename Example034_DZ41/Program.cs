a5577d6c4ad375efb776d26daa685c26c7b20f96
Krasnov Vitaliy <vitgard82@mail.ru>
2023-02-17 10:23:05 +0600

    нашел числа больше нуля

---
+++ Example034_DZ41/Program.cs
@@ -0,0 +1,18 @@
+﻿//Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+
+Console.Clear();
+
+Console.WriteLine("Какое количество цифр вы хотите ввести?");
+int m = int.Parse(Console.ReadLine()!);
+int count = 0;
+
+for (int i = 0; i < m; i++)
+{
+    Console.WriteLine("Введите число ");
+    int number = int.Parse(Console.ReadLine()!);
+    if (number > 0)
+    count++;
+}
+
+Console.WriteLine($"Количество чисел больше 0: {count}");
